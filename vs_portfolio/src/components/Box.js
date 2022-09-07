a044513514f064362bb3701b381bbde169d5db83
Tvallejoss <Tvallejos@stgroup.com.ar>
2022-09-07 19:26:22 -0300

    feat: avances en maquetacion

---
+++ vs_portfolio/src/components/Box.js
@@ -0,0 +1,149 @@
+import React from "react";
+import "../App.css";
+
+export const Box = () => {
+    return (
+            <div className="box">
+                <div className="firstTab">
+                    <ul>
+                        <li>
+                            {" "}
+                            <img
+                                src="https://o.remove.bg/downloads/595ec232-afd8-4232-8fae-05b8b6799361/Visual_Studio_Code_1.35_icon.svg-removebg-preview.png"
+                                alt="VS"
+                            />{" "}
+                        </li>
+                        <li>File</li>
+                        <li>Edit</li>
+                        <li>Selection</li>
+                        <li>View</li>
+                        <li>Go</li>
+                        <li>Run</li>
+                        <li>Terminal</li>
+                        <li>Help</li>
+                    </ul>
+                </div>
+
+                <div className="proyectName">
+                    <h1>
+                        {" "}
+                        TomasVallejos.js - vs_portfolio - Visual Studio Code
+                    </h1>
+                </div>
+                <div className="secondTab">
+                    <ul>
+                        <li>
+                            {" "}
+                            <img
+                                src="https://cdn.discordapp.com/attachments/840217064978907170/1017176923547381790/logoOne.png"
+                                alt="a"
+                            />
+                        </li>
+                        <li>
+                            {" "}
+                            <img
+                                src="https://cdn.discordapp.com/attachments/840217064978907170/1017178175580688435/logoTwo.png"
+                                alt="a"
+                            />
+                        </li>
+                        <li>
+                            {" "}
+                            <img
+                                src="https://cdn.discordapp.com/attachments/840217064978907170/1017178198745817188/logothree.png"
+                                alt="a"
+                            />
+                        </li>
+                        {/* <li>|</li> */}
+                        <li>
+                            {" "}
+                            <img
+                                src="https://cdn.discordapp.com/attachments/840217064978907170/1017178216382865469/logoFour.png"
+                                alt="a"
+                            />
+                        </li>
+                        <li>
+                            {" "}
+                            <img
+                                src="https://cdn.discordapp.com/attachments/840217064978907170/1017182197582659676/menos.png"
+                                alt="a"
+                                className="iconos"
+                            />
+                        </li>
+                        <li>
+                            {" "}
+                            <img
+                                src="https://cdn.discordapp.com/attachments/840217064978907170/1017181570337079377/cuadrado.png"
+                                alt="a"
+                                className="iconos"
+                            />
+                        </li>
+                        <li>
+                            {" "}
+                            <img
+                                src="https://cdn.discordapp.com/attachments/840217064978907170/1017182221582487634/x.png"
+                                alt="a"
+                                className="iconos"
+                            />
+                        </li>
+                    </ul>
+                </div>
+
+                <div className="content">
+                    
+                    <div className="sidebar">
+
+                        <aside>
+                            <ul>
+                                <li>O</li>
+                                <li>0</li>
+                                <li>0</li>
+                                <li>0</li>
+                                <li>0</li>
+                                <li>0</li>
+                                <li>0</li>
+                                <li>0</li>
+                            </ul>
+
+                        </aside>
+                    </div>
+
+                    <div className="asideFolder">
+                        
+                        <ul>
+                            <li><p>Explorer ...</p></li>
+
+                            <li>VS_PORTFOLIO</li>
+                            <li>
+                                <ul>
+                                    <li>Folder</li>
+                                    <li>Folder</li>
+                                    <li>File</li>
+                                    <li>Folder</li>
+                                    <li>Folder</li>
+                                </ul>
+                            </li>
+                        </ul>
+                        </div>  
+
+
+
+                    <div className="principalContent">
+                        
+                    </div>
+                        
+
+                </div>
+
+
+            </div>
+
+            
+
+             
+
+
+         
+      
+        
+    );
+};
